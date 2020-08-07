a7a8e95eb8e53976ea2b26f8c0639e2b95df6327
kazorkthedork <69063645+kazorkthedork@users.noreply.github.com>
2020-08-07 17:07:23 -0400

    added banners????
    bºners

---
+++ Thomas and I doing stupid stuff/Assets/BannerTrackPlanet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BannerTrackPlanet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public Transform planetPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=planetPosition.position;
+        transform.position = transform.position + new Vector3(0,10,0);
+    }
+}
