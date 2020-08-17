d0b79cef1bc95c033b8bad080daf46e13ff2d2d8
Evyn <evynster@hotmail.com>
2020-08-16 21:22:39 -0400

    Adding stuff

---
+++ Thomas and I doing stupid stuff/Assets/Scripts/RandomMakeSolarSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class RandomMakeSolarSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+    public Vector<int> temp;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
