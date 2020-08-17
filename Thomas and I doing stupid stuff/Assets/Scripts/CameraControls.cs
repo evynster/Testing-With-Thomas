3acd6f90e773f9de1f342bfb532c49b1f05a0182
Evyn <evynster@hotmail.com>
2020-08-16 22:48:56 -0400

    Stole A camera

--- Thomas and I doing stupid stuff/Assets/Scripts/CameraControls.cs
+++ Thomas and I doing stupid stuff/Assets/Scripts/CameraControls.cs
@@ -1,95 +1,62 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
+[AddComponentMenu("Camera-Control/Smooth Mouse Look")]
 public class CameraControls : MonoBehaviour
 {
-    //brackeys told me to do this
-    public float mouseSensitivity = 100f;
-    public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -80F;
+    public float maximumY = 80F;
+    float rotationX = 0F;
+    float rotationY = 0F;
+    public float frameCounter = 20;
+    Quaternion originalRotation;
     void Update()
     {
+        if (Input.GetKey(KeyCode.P))
+            Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKey(KeyCode.O))
+            Cursor.lockState = CursorLockMode.None;
 
-        //Mouse looking code.  gets the x and y mouse movement and applies it to the rotational vector of the camera.
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity *10 *Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity *10 *Time.deltaTime;
-        
+        //Gets rotational input from the mouse
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
 
-        transform.Rotate(Vector3.up * mouseX);
-        transform.Rotate(Vector3.left * mouseY);
-        //any Z rotation from the mouse is then clamped out here.
-        Vector3 mouseLook = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, 0);
-        //transform.leulerAngles = mouseLook;
-        //mouseLook.x = Mathf.Clamp(mouseLook.x, 0f,89.5f );
+        //Clamp the rotation average to be within a specific value range
+        rotationY = ClampAngle(rotationY, minimumY, maximumY);
+        rotationX = ClampAngle(rotationX, minimumX, maximumX);
 
-       // mouseLook.x = Mathf.Clamp(mouseLook.x, 0, 150);
-        if(mouseLook.x >85f && mouseLook.x < 90f)
-        {
-            mouseLook.x = 84.9999999999999999f;
-        }
-        else if (mouseLook.x < 275f && mouseLook.x > 270f)
-        {
-            mouseLook.x = 275.00000000000000001f;
-        }
-         
-        transform.localRotation = Quaternion.Euler(mouseLook.x, mouseLook.y, 0f);
-        Debug.Log("mouse raw input "+ mouseX + mouseY + "mouseLook v3 x,y: " + mouseLook.x +" , "+ mouseLook.y);
-        cameraTranslationSpeed();
-        cameraKeyMovement();
-        
-    }
-    void cameraTranslationSpeed() {
-        if (Input.mouseScrollDelta.y > 0)
-        {
-            speed++;
-        }
-        else if (Input.mouseScrollDelta.y < 0 && speed > 0)
-        {
-            speed--;
-        }
+        //Get the rotation you will be at next as a Quaternion
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+
+        //Rotate
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
 
     }
-    void cameraKeyMovement() {
-        //keyboard rotation 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(Vector3.forward * 0.05f);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(Vector3.back * 0.05f);
-        }
-        //keyboard translation 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(speed * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(0f, speed * Time.deltaTime, 0f);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            transform.Translate(0f, -speed * Time.deltaTime, 0f);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0f, 0f, -speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0f, 0f, speed * Time.deltaTime);
-        }
+    void Start()
+    {
+        originalRotation = transform.localRotation;
     }
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F))
+        {
+            if (angle < -360F)
+            {
+                angle += 360F;
+            }
+            if (angle > 360F)
+            {
+                angle -= 360F;
+            }
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+
+
 }
